c120ab1e8f34bcc3cb115b2ee18f53a73a239608
Midomik <krastikrabbs9234@gmail.com>
2024-04-10 11:32:31 +0300

    implement/saving-template-repo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>Ping Pong !!!</div>
+    // <SharedLayout>
+    //   <Routes>
+    //     {/* <Route index path="/" element={<WelcomePage />} /> */}
+
+    //   </Routes>
+    // </SharedLayout>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+// import { BrowserRouter } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+// import { persistor, store } from './redux/store';
+// import { PersistGate } from 'redux-persist/integration/react';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <BrowserRouter basename="/template-repo-from-create-react-app">
+      <Provider store={store}>
+        <PersistGate persistor={persistor}> */}
+    <App />
+    {/* </PersistGate>
+      </Provider>
+    </BrowserRouter> */}
+  </React.StrictMode>
+);
